4fe70cb85d6ab5aa03aeb12c6e515eb4f247a927
dcmacedo <dcotrimmacedo@gmail.com>
2024-08-08 20:49:52 -0300

    Final Commit

--- src/functional_interface/examples/PredicateExample.java
+++ src/functional_interface/examples/PredicateExample.java
@@ -9,17 +9,17 @@ import java.util.function.Predicate;
  * É comumente usada para filtrar os elementos do Stream com base em alguma condição.
  */
 public class PredicateExample {
-  public static void main(String[] args) {
-    // Criar uma lista de palavras
-    List<String> palavras = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
+    public static void main(String[] args) {
+        // Criar uma lista de palavras
+        List<String> palavras = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
 
-    // Criar um Predicate que verifica se a palavra tem mais de 5 caracteres
-    Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+        // Criar um Predicate que verifica se a palavra tem mais de 5 caracteres
+        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
 
-    // Usar o Stream para filtrar as palavras com mais de 5 caracteres e, em seguida,
-    // imprimir cada palavra que passou no filtro
-    palavras.stream()
-        .filter(maisDeCincoCaracteres)
-        .forEach(System.out::println);
-  }
+        // Usar o Stream para filtrar as palavras com mais de 5 caracteres e, em seguida,
+        // imprimir cada palavra que passou no filtro
+        palavras.stream()
+                .filter(p -> p.length() < 5)
+                .forEach(System.out::println);
+    }
 }
--- src/functional_interface/examples/SupplierExample.java
+++ src/functional_interface/examples/SupplierExample.java
@@ -9,16 +9,16 @@ import java.util.stream.Stream;
  * É comumente usada para criar ou fornecer novos objetos de um determinado tipo.
  */
 public class SupplierExample {
-  public static void main(String[] args) {
-    // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
-    Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
+    public static void main(String[] args) {
+        // Usar o Supplier com expressão lambda para fornecer uma saudação personalizada
+        Supplier<String> saudacao = () -> "Olá, seja bem-vindo(a)!";
 
-    // Usar o Supplier para obter uma lista com 5 saudações
-    List<String> listaSaudacoes = Stream.generate(saudacao)
-        .limit(5)
-        .toList();
+        // Usar o Supplier para obter uma lista com 5 saudações
+        List<String> listaSaudacoes = Stream.generate(() ->  "Olá, seja bem-vindo(a)!")
+                .limit(6)
+                .toList();
 
-    // Imprimir as saudações geradas
-    listaSaudacoes.forEach(System.out::println);
-  }
+        // Imprimir as saudações geradas
+        listaSaudacoes.forEach(System.out::println);
+    }
 }
